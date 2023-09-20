496b589270069b0ce983343c669559f52eb51cc5
aanassor1 <abdulaziz.nassor1@gmail.com>
2023-09-20 16:49:44 +0100

    Added files

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.Message = new System.Windows.Forms.Label();
+            this.Grid = new System.Windows.Forms.Panel();
+            this.ButtonBottomRight = new System.Windows.Forms.Button();
+            this.ButtonBottomCentre = new System.Windows.Forms.Button();
+            this.ButtonBottomLeft = new System.Windows.Forms.Button();
+            this.ButtonMiddleRight = new System.Windows.Forms.Button();
+            this.ButtonMiddleCentre = new System.Windows.Forms.Button();
+            this.ButtonMiddleLeft = new System.Windows.Forms.Button();
+            this.ButtonTopRight = new System.Windows.Forms.Button();
+            this.ButtonTopMiddle = new System.Windows.Forms.Button();
+            this.ButtonTopLeft = new System.Windows.Forms.Button();
+            this.Retry = new System.Windows.Forms.Button();
+            this.Grid.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Message
+            // 
+            this.Message.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Message.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Message.Location = new System.Drawing.Point(13, 7);
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(134, 20);
+            this.Message.TabIndex = 4;
+            this.Message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Grid
+            // 
+            this.Grid.BackgroundImage = global::TicTacToe.Properties.Resources.kisspng_css_grid_layout_mathematics_game_law_offices_of_le_tic_tac_toe_the_classic_game_5b3bda9d3dec24_3962301915306492452537;
+            this.Grid.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Grid.Controls.Add(this.ButtonBottomRight);
+            this.Grid.Controls.Add(this.ButtonBottomCentre);
+            this.Grid.Controls.Add(this.ButtonBottomLeft);
+            this.Grid.Controls.Add(this.ButtonMiddleRight);
+            this.Grid.Controls.Add(this.ButtonMiddleCentre);
+            this.Grid.Controls.Add(this.ButtonMiddleLeft);
+            this.Grid.Controls.Add(this.ButtonTopRight);
+            this.Grid.Controls.Add(this.ButtonTopMiddle);
+            this.Grid.Controls.Add(this.ButtonTopLeft);
+            this.Grid.Location = new System.Drawing.Point(11, 33);
+            this.Grid.Name = "Grid";
+            this.Grid.Size = new System.Drawing.Size(228, 232);
+            this.Grid.TabIndex = 1;
+            // 
+            // ButtonBottomRight
+            // 
+            this.ButtonBottomRight.FlatAppearance.BorderSize = 0;
+            this.ButtonBottomRight.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonBottomRight.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonBottomRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonBottomRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonBottomRight.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonBottomRight.Location = new System.Drawing.Point(159, 163);
+            this.ButtonBottomRight.Name = "ButtonBottomRight";
+            this.ButtonBottomRight.Size = new System.Drawing.Size(67, 70);
+            this.ButtonBottomRight.TabIndex = 8;
+            this.ButtonBottomRight.UseVisualStyleBackColor = true;
+            // 
+            // ButtonBottomCentre
+            // 
+            this.ButtonBottomCentre.FlatAppearance.BorderSize = 0;
+            this.ButtonBottomCentre.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonBottomCentre.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonBottomCentre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonBottomCentre.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonBottomCentre.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonBottomCentre.Location = new System.Drawing.Point(82, 163);
+            this.ButtonBottomCentre.Name = "ButtonBottomCentre";
+            this.ButtonBottomCentre.Size = new System.Drawing.Size(67, 70);
+            this.ButtonBottomCentre.TabIndex = 7;
+            this.ButtonBottomCentre.UseVisualStyleBackColor = true;
+            // 
+            // ButtonBottomLeft
+            // 
+            this.ButtonBottomLeft.FlatAppearance.BorderSize = 0;
+            this.ButtonBottomLeft.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonBottomLeft.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonBottomLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonBottomLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonBottomLeft.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonBottomLeft.Location = new System.Drawing.Point(5, 163);
+            this.ButtonBottomLeft.Name = "ButtonBottomLeft";
+            this.ButtonBottomLeft.Size = new System.Drawing.Size(67, 70);
+            this.ButtonBottomLeft.TabIndex = 6;
+            this.ButtonBottomLeft.UseVisualStyleBackColor = true;
+            // 
+            // ButtonMiddleRight
+            // 
+            this.ButtonMiddleRight.FlatAppearance.BorderSize = 0;
+            this.ButtonMiddleRight.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonMiddleRight.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonMiddleRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonMiddleRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonMiddleRight.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonMiddleRight.Location = new System.Drawing.Point(158, 81);
+            this.ButtonMiddleRight.Name = "ButtonMiddleRight";
+            this.ButtonMiddleRight.Size = new System.Drawing.Size(67, 70);
+            this.ButtonMiddleRight.TabIndex = 5;
+            this.ButtonMiddleRight.UseVisualStyleBackColor = true;
+            // 
+            // ButtonMiddleCentre
+            // 
+            this.ButtonMiddleCentre.FlatAppearance.BorderSize = 0;
+            this.ButtonMiddleCentre.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonMiddleCentre.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonMiddleCentre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonMiddleCentre.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonMiddleCentre.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonMiddleCentre.Location = new System.Drawing.Point(81, 81);
+            this.ButtonMiddleCentre.Name = "ButtonMiddleCentre";
+            this.ButtonMiddleCentre.Size = new System.Drawing.Size(67, 70);
+            this.ButtonMiddleCentre.TabIndex = 4;
+            this.ButtonMiddleCentre.UseVisualStyleBackColor = true;
+            // 
+            // ButtonMiddleLeft
+            // 
+            this.ButtonMiddleLeft.FlatAppearance.BorderSize = 0;
+            this.ButtonMiddleLeft.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonMiddleLeft.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonMiddleLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonMiddleLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonMiddleLeft.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonMiddleLeft.Location = new System.Drawing.Point(4, 81);
+            this.ButtonMiddleLeft.Name = "ButtonMiddleLeft";
+            this.ButtonMiddleLeft.Size = new System.Drawing.Size(67, 70);
+            this.ButtonMiddleLeft.TabIndex = 3;
+            this.ButtonMiddleLeft.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTopRight
+            // 
+            this.ButtonTopRight.FlatAppearance.BorderSize = 0;
+            this.ButtonTopRight.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonTopRight.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonTopRight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTopRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonTopRight.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonTopRight.Location = new System.Drawing.Point(159, 1);
+            this.ButtonTopRight.Name = "ButtonTopRight";
+            this.ButtonTopRight.Size = new System.Drawing.Size(67, 70);
+            this.ButtonTopRight.TabIndex = 2;
+            this.ButtonTopRight.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTopMiddle
+            // 
+            this.ButtonTopMiddle.FlatAppearance.BorderSize = 0;
+            this.ButtonTopMiddle.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonTopMiddle.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonTopMiddle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTopMiddle.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonTopMiddle.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonTopMiddle.Location = new System.Drawing.Point(82, 1);
+            this.ButtonTopMiddle.Name = "ButtonTopMiddle";
+            this.ButtonTopMiddle.Size = new System.Drawing.Size(67, 70);
+            this.ButtonTopMiddle.TabIndex = 1;
+            this.ButtonTopMiddle.UseVisualStyleBackColor = true;
+            // 
+            // ButtonTopLeft
+            // 
+            this.ButtonTopLeft.FlatAppearance.BorderSize = 0;
+            this.ButtonTopLeft.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumPurple;
+            this.ButtonTopLeft.FlatAppearance.MouseOverBackColor = System.Drawing.Color.SlateBlue;
+            this.ButtonTopLeft.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonTopLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.ButtonTopLeft.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonTopLeft.Location = new System.Drawing.Point(5, 1);
+            this.ButtonTopLeft.Name = "ButtonTopLeft";
+            this.ButtonTopLeft.Size = new System.Drawing.Size(67, 70);
+            this.ButtonTopLeft.TabIndex = 0;
+            this.ButtonTopLeft.UseVisualStyleBackColor = true;
+            // 
+            // Retry
+            // 
+            this.Retry.FlatAppearance.BorderSize = 0;
+            this.Retry.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Retry.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Retry.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Retry.Location = new System.Drawing.Point(162, 7);
+            this.Retry.Name = "Retry";
+            this.Retry.Size = new System.Drawing.Size(75, 21);
+            this.Retry.TabIndex = 5;
+            this.Retry.Text = "Retry";
+            this.Retry.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(250, 277);
+            this.Controls.Add(this.Retry);
+            this.Controls.Add(this.Message);
+            this.Controls.Add(this.Grid);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.Grid.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label Message;
+        private System.Windows.Forms.Panel Grid;
+        private System.Windows.Forms.Button ButtonTopLeft;
+        private System.Windows.Forms.Button ButtonTopMiddle;
+        private System.Windows.Forms.Button ButtonTopRight;
+        private System.Windows.Forms.Button ButtonBottomRight;
+        private System.Windows.Forms.Button ButtonBottomCentre;
+        private System.Windows.Forms.Button ButtonBottomLeft;
+        private System.Windows.Forms.Button ButtonMiddleRight;
+        private System.Windows.Forms.Button ButtonMiddleCentre;
+        private System.Windows.Forms.Button ButtonMiddleLeft;
+        private Button Retry;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            initialiseGame();
+        }
+
+        int Turn = 1; //    Variable used to track which player's go it is
+
+        private void initialiseGame()
+        {
+            this.Retry.Visible = false;
+            this.Retry.Click -= RetryButtonClick;
+            Turn = 1;
+            foreach (Button button in this.Grid.Controls)
+            {
+                button.Text = "";
+                button.FlatAppearance.MouseOverBackColor = Color.MediumPurple;
+                button.FlatAppearance.MouseDownBackColor = Color.SlateBlue;
+                button.Click += new EventHandler(gridButtonClick); //   Create a click event
+            }
+            Message.Text = "Player 1's Turn";
+        }
+
+        private void disableHover(Button btn)
+        {
+            btn.FlatAppearance.MouseOverBackColor = btn.BackColor;
+            btn.FlatAppearance.MouseDownBackColor = btn.BackColor;
+            btn.Click -= gridButtonClick;   // Delete the click event
+        }
+
+        //  Function that checks if the given array has a winning combination or not
+        private bool won(Button[] buttons, string letter)
+        {
+            //  Contains array positions of winning combinations
+            int[][] winningCombinations = {
+              new []{0, 1, 2},
+              new []{3, 4, 5},
+              new []{6, 7, 8},
+              new []{0, 3, 6},
+              new []{1, 4, 7},
+              new []{2, 5, 8},
+              new []{0, 4, 8},
+              new []{2, 4, 6},
+            };
+
+            bool found = false;
+            for (int i = 0; i < winningCombinations.Length; i++)
+            {
+                int x = winningCombinations[i][0];
+                int y = winningCombinations[i][1];
+                int z = winningCombinations[i][2];
+                if (buttons[x].Text == letter && buttons[y].Text == letter && buttons[z].Text == letter)
+                {
+                    found = true;
+                }
+            }
+
+            return found;
+        }
+
+        //  Function that checks if the current grid is a draw or not
+        private bool draw(Button[] buttons)
+        {
+            int notEmpty = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                if (buttons[i].Text != "")
+                {
+                    notEmpty++;
+                }
+            }
+            if (notEmpty == 9) {
+                return true;
+            }
+            return false;
+        }
+
+        //  Used to check if the game has been concluded or not
+        private void checkGameStatus()
+        {
+            string letter;
+            string player;
+            if (Turn == 2) { // Make the letter opposite to check previous turn
+                letter = "X";
+                player = "Player 1";
+            } else {
+                letter = "O";
+                player = "Player 2";
+            }
+
+            //  Initialise a buttons array to make a grid
+            Button[] buttons = new Button[9];
+            String[] buttonNames = {
+                "ButtonTopLeft", "ButtonTopMiddle", "ButtonTopRight",
+                "ButtonMiddleLeft", "ButtonMiddleCentre", "ButtonMiddleRight",
+                "ButtonBottomLeft", "ButtonBottomCentre", "ButtonBottomRight"
+            };
+            for (int i = 0; i < 9; i++)
+            {
+                buttons[i] = (Button)this.Grid.Controls[buttonNames[i]];
+            }
+
+            //  If game is concluded, finish the game and make the retry button visible
+            if (won(buttons, letter) == true)
+            {
+                Message.Text = player + "'s won!";
+                foreach (Button button in this.Grid.Controls)
+                {
+                    disableHover(button);
+                }
+                this.Retry.Visible = true;
+                this.Retry.Click += new EventHandler(RetryButtonClick);
+            } else if (draw(buttons) == true)
+            {
+                Message.Text = "No one won.";
+                foreach (Button button in this.Grid.Controls)
+                {
+                    disableHover(button);
+                }
+                this.Retry.Visible = true;
+                this.Retry.Click += new EventHandler(RetryButtonClick);
+            }
+        }
+
+        private void RetryButtonClick(object sender, EventArgs e)
+        {
+            initialiseGame();
+        }
+
+        //  Handles click events for the main grid
+        private void gridButtonClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (Turn == 1)
+            {
+                btn.Text = "X";
+                btn.ForeColor = SystemColors.ControlText;
+                disableHover(btn);
+                Message.Text = "Player 2's Turn";
+                Turn = 2;
+            } else
+            {
+                btn.Text = "O";
+                btn.ForeColor = SystemColors.ControlLight;
+                disableHover(btn);
+                Message.Text = "Player 1's Turn";
+                Turn = 1;
+            }
+            checkGameStatus();
+            btn.Click -= gridButtonClick; //    Disable the button once it has been clicked
+        }
+    }
+}
